7150b38dfc4d510657e61a9e560d9623238d8964
marvikomo <marvikomo@gmail.com>
2023-10-19 21:26:30 +0100

    updated file helper

--- helper/files.js
+++ helper/files.js
@@ -7,6 +7,11 @@ const getFiles = (dir, fileList = []) => {
     const files = fs.readdirSync(dir)
     console.log(files)
     files.forEach(file => {
-
+        const filePath = path.join(dir, file);
+        if (fs.statSync(filePath).isDirectory()) {
+            getAllFiles(filePath, fileList);
+        } else {
+            fileList.push(filePath);
+        }
     })
 }
