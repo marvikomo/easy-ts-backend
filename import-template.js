c7f63375df3097eafbcddc42f8d982935accd666
marvikomo <marvikomo@gmail.com>
2023-10-19 20:04:12 +0100

    added import-template

---
+++ import-template.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
+
+
+module.exports.importTemplate = async function importTemplate(args) {
+  console.log("args", args)    
+}
