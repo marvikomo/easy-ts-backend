e424f9ec64c978afe37df2b161eccb1430b6da95
marvikomo <marvikomo@gmail.com>
2023-10-19 21:24:28 +0100

    modified import-template to copy templateDir

--- import-template.js
+++ import-template.js
@@ -1,8 +1,26 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const ejs = require('ejs');
 
 
 module.exports.importTemplate = async function importTemplate(args) {
-  console.log("args", args)    
+    const templateDir = path.join(__dirname, 'templates');
+    const targetDir = process.cwd()+ "/test" 
+    if(fs.existsSync(targetDir)) throw new Error("Template already imported or make sure your current directory is empty")
+    fs.copySync(templateDir, targetDir);
+    console.log('Template imported successfully!');
+}
+
+
+const checkTemplateHasBeenImported = (targetDir, templateDir) => {
+     
+}
+
+
+const getFiles = (dir, fileList = []) => {
+    const files = fs.readdirSync(dir)
+    console.log(files)
+    files.forEach(file => {
+
+    })
 }
