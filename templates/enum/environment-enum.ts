e75a341dab80e21f29fa101eaaa8d9dc339203d8
marvikomo <marvikomo@gmail.com>
2023-11-02 23:52:40 +0100

    added environment enum

---
+++ templates/enum/environment-enum.ts
@@ -0,0 +1,14 @@
+/**
+ * @enum Environment
+ * @property {string} DEVELOPMENT
+ * @property {string} TEST
+ * @property {string} PRODUCTION
+ * @memberOf Constants
+ */
+enum Environment {
+    DEVELOPMENT = "development",
+    TEST = "test",
+    PRODUCTION = "production",
+  }
+  
+  export default Environment;
---
+++ templates/enum/index.ts
@@ -0,0 +1,5 @@
+import Environment from "./environment-enum"
+
+export {
+    Environment
+}
